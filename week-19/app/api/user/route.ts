a0e651aefbcaff0a88abb60e2426e1fc84c61081
yash <yashvikram8250@gmail.com>
2025-01-07 22:14:19 +0530

    get route implementation

---
+++ week-19/app/api/user/route.ts
@@ -0,0 +1,3 @@
+export async function GET(){
+    return Response.json({username: "yash", email: "yash@gmail.com"})
+}
---
+++ week-19/app/signup/page.tsx
@@ -0,0 +1,5 @@
+import { Signup } from "../components/page"
+
+export default function Home() {
+    return <Signup />
+}
